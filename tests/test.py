08562b8c1d37bcb3455d0eb06063cd4a554741f3
SimonPfeifer <sikon08@hotmail.co.uk>
2022-01-07 18:56:05 +0100

    Moved the temporary test file.

--- tests/test.py
+++ tests/test.py
@@ -5,12 +5,12 @@ ncells = 10
 
 # Set up some example data
 data = np.zeros([ncells,ncells,ncells])
-data[1:9, 3:8, 3:8] = 1
-data[3:8, 1:9, 3:8] = 1
+data[1:9, 4:7, 4:7] = 1
+data[4:7, 1:9, 4:7] = 1
 # data[3:8, 3:8, 1:9] = 1
 
 # Generate the skeleton
-skeleton = cows.skeletonize(data)
+skeleton = cows.skeletonize(data, periodic=False)
 
 # Separate the skeleton
 filaments = cows.separate_skeleton(skeleton)
